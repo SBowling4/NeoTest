3d0fbac7d98c2435df13e93b59fa70516ede65bd
SBowling4 <bowlisam001@icloud.com>
2024-09-13 00:20:07 +0100

    fix

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -20,7 +20,7 @@ public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
   private RobotContainer m_robotContainer;
-  private CommandXboxController xbox = new CommandXboxController(0);
+  private CommandXboxController xbox = new CommandXboxController(1);
   private PWMSparkMax motor = new PWMSparkMax(0);
   /**
    * This function is run when the robot is first started up and should be used for any
